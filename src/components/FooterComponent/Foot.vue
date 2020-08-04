15a3c162db2c47222d72521218219f2f5f60ed01
18616346183 <liu3519@qq.com>
2020-08-04 17:51:29 +0800

    增加回到主页选项

--- src/components/FooterComponent/Foot.vue
+++ src/components/FooterComponent/Foot.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="foot-main">
     <p>Written by cyt&amp;lxt</p>
-    <p>
-      沪ICP备XXXXXXXX号
-    </p>
+
+    <p>沪ICP备XXXXXXXX号&nbsp;<a href="javascript:void(0);" @click="backToFirst()">回到主页</a></p>
   </div>
 </template>
 <script>
 export default {
-  name: 'FootPage',
+  name: "FootPage",
   data() {
-    return {
-    };
-  }
-}
+    return {};
+  },
+  methods: {
+    backToFirst() {
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 <style lang="scss" scope>
 .foot-main {
-    background-color: beige;
-    opacity: 0.5;
-    margin-top: -15px;
-    text-align: center;
-    font-size: 12px;
+  background-color: beige;
+  opacity: 0.5;
+  margin-top: -15px;
+  text-align: center;
+  font-size: 12px;
 }
 </style>
